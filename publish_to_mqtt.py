7ac7abdf645689d7f85a736b5ca65d3ca347574e
Anton Nanchev <ananchev@gmail.com>
2024-03-16 11:42:00 +0100

    update python publish script

--- publish_to_mqtt.py
+++ publish_to_mqtt.py
@@ -1,39 +1,58 @@
 import serial, minimalmodbus
 
 usb2s = '/dev/ttyUSB0'
-instrument = minimalmodbus.Instrument(usb2s,102) # port name, Broadcast slave address
-instrument.serial.baudrate = 4800      
+
+instrument = minimalmodbus.Instrument(usb2s,102) # slave 102 is the grass area sensor 
+
+instrument.serial.baudrate = 9600
 instrument.serial.bytesize = 8
 instrument.serial.stopbits = 1
 instrument.serial.parity   = serial.PARITY_NONE
 instrument.mode = minimalmodbus.MODE_RTU 
 instrument.serial.timeout = 0.5
 
-moisture = instrument.read_register(0,0)
-print(f" moisture reading: {moisture} %")
-temperature = instrument.read_register(1,0)
-print(f" temperature reading: {temperature} {chr(176)}C")
+ga_moisture = instrument.read_register(0,0)
+ga_temperature = instrument.read_register(1,0)
 
+# time.sleep(0.1) # small timeout to avoid minimalmodbus.NoResponseError
+# instrument.address = 103 # slave 103 is the flower garden sensor
+# fg_moisture = instrument.read_register(0,0)
+# fg_temperature = instrument.read_register(1,0)
+
+# print(f" grass area moisture reading: {ga_moisture} %")
+# print(f" grass area temperature reading: {ga_temperature} {chr(176)}C")
+# print(f" flower garden moisture reading: {fg_moisture} %")
+# print(f" flower temperature reading: {fg_temperature} {chr(176)}C")
 
 import paho.mqtt.publish as paho_publisher
 
 mqtt_server = "192.168.2.8"
 mqtt_port = 1883
-mqtt_topic = "hm-achter"
-mqtt_client="tmp_achtertuin_hm"
+mqtt_topic = "backyard_soil_sensors"
+mqtt_client="achtertuin_hm"
+
+
 
 msgs = [
+         #  {
+         #      "topic":f"{mqtt_topic}/l4",
+         #      "payload":fg_temperature
+         #  },
+         #  {
+         #      "topic":f"{mqtt_topic}/l5",
+         #      "payload":fg_moisture
+         #  },
            {
-               "topic":"hm-achter/moisture",
-               "payload":moisture
+               "topic":f"{mqtt_topic}/l1",
+               "payload":ga_moisture
            },
            {
-               "topic":"hm-achter/temperature",
-               "payload":temperature
+               "topic":f"{mqtt_topic}/l3",
+               "payload":ga_temperature
            }
        ]
 
-paho_publisher.multiple(msgs, 
-                       hostname="192.168.2.8", 
-                       port=1883, 
-                       client_id="tmp_achtertuin_hm")
+paho_publisher.multiple(msgs,
+                       hostname=mqtt_server,
+                       port=mqtt_port,
+                       client_id=mqtt_client)
